51ac98dff4993a51402fa67e37b418f75cb625e1
Evan <evanfujita@gmail.com>
2021-02-09 19:32:57 -0600

    commit tree 0e15ad873a5d485b5b61418b3f7ad32934018f84

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-const num1 = 2
-const num2 = 31
+const num1 = 31
+const num2 = 2
+
 const multiply = num1 * num2
 
-const random = Math.floor(Math.random()*10 + 1)
+const random = Math.floor(Math.random()* 10) + 1
 
-const num3 = 13
-const num4 = 9
-const mod = num3 % num4
+const num3 = 9
+const num4 = 5
 
-const max = Math.max(1, 2, 20)
+const mod = num3%num4
+
+const max = Math.max(1, 4, 3, 2, 6, 20)
